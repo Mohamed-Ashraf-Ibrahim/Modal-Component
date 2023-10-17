ee32fe82b1f87764b2f5f320bdc92832445e145d
Mohamed Ashraf <146873948+Mohamed-Ashraf-Ibrahim@users.noreply.github.com>
2023-10-17 04:35:18 +0300

    Add files via upload

---
+++ Modal Component/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const subBtn = document.getElementById("submit");
+const closeBtn = document.getElementById("close-btn");
+const modal = document.getElementById("modal");
+const overlay = document.getElementById("overlay");
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+const OpenModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+subBtn.addEventListener("click", function () {
+  OpenModal();
+});
+
+closeBtn.addEventListener("click", function () {
+  closeModal();
+});
+
+overlay.addEventListener("click", function () {
+  closeModal();
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
+});
